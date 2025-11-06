42ff13d61db4ee4828663bcc7695cc87a11e166e
Anastasia <161876455+anastasiacheb@users.noreply.github.com>
2025-11-06 21:46:12 +0300

    pwa color

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,19 @@ import tailwindcss from '@tailwindcss/vite';
 import { VitePWA } from 'vite-plugin-pwa';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Quitchen | Fresh Sushi & Japanese Cuisine',
+        short_name: 'Quitchen',
+        theme_color: '#0a0b0a',
+        background_color: '#0a0b0a',
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './'),
