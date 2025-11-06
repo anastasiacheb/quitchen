6f6d82dfe308477cc9f65306cba6e8dc60995e3c
Anastasia <161876455+anastasiacheb@users.noreply.github.com>
2025-11-06 23:16:00 +0300

    pwa

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,10 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
       manifest: {
         name: 'Quitchen | Fresh Sushi & Japanese Cuisine',
         short_name: 'Quitchen',
